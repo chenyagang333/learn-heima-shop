8cb3bc28bcef06779a7b687db76df872fb378056
chenyagang <2578036363@qq.com>
2024-08-03 15:43:45 +0800

    feat: 轮播图指示点

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
-//
+import CustomNavbar from './components/CustomNavBar.vue'
 </script>
 
 <template>
+  <!-- 自定义导航栏 -->
+  <CustomNavbar />
+  <!-- 自定义轮播图 -->
+  <XtxSwiper />
   <view class="index">index</view>
 </template>
 
