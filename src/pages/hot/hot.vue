e0c93e6c17a2d67e422fa0e0761712c1bd6352dd
chenyagang <2578036363@qq.com>
2024-08-03 19:11:14 +0800

    feat: 热门数据推荐的获取以及类型的定义

--- src/pages/hot/hot.vue
+++ src/pages/hot/hot.vue
@@ -1,5 +1,8 @@
 // /src/pages/hot/hot.vue
 <script setup lang="ts">
+import { getHotRecommendAPI } from '@/services/hot'
+import { onLoad } from '@dcloudio/uni-app'
+
 // 热门推荐页 标题和url
 const hotMap = [
   { type: '1', title: '特惠推荐', url: '/hot/preference' },
@@ -8,6 +11,7 @@ const hotMap = [
   { type: '4', title: '新鲜好物', url: '/hot/new' },
 ]
 
+// 小程序页面获取参数的方法，和组件一致
 const query = defineProps<{
   type: string
 }>()
@@ -15,6 +19,15 @@ const query = defineProps<{
 const currUrlMap = hotMap.find((h) => h.type === query.type)
 // 动态设置标题
 uni.setNavigationBarTitle({ title: currUrlMap!.title })
+// 获取热门推荐数据
+const getHotRecommendData = async () => {
+  const res = await getHotRecommendAPI(currUrlMap!.url)
+}
+
+// 页面加载
+onLoad(() => {
+  getHotRecommendData()
+})
 </script>
 
 <template>
--- src/types/home.d.ts
+++ src/types/home.d.ts
@@ -37,19 +37,8 @@ export type HotItem = {
 }
 
 /** 猜你喜欢-商品类型 */
-export type GuessItem = {
-  /** 商品描述 */
-  desc: string
-  /** 商品折扣 */
-  discount: number
-  /** id */
-  id: string
-  /** 商品名称 */
-  name: string
-  /** 商品已下单数量 */
-  orderNum: number
-  /** 商品图片 */
-  picture: string
-  /** 商品价格 */
-  price: number
-}
+// src/services/home.ts
+import type { GoodsItem } from '@/types/global'
+
+// GuessItem 和 GoodsItem 类型相同
+export type GuessItem = GoodsItem
