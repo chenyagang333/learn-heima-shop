4e188f63201bd22ba0f05d09f6f80ef8d32ded84
chenyagang <2578036363@qq.com>
2024-08-04 12:02:58 +0800

    feat: 会员中心-会员信息渲染

--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -31,7 +31,7 @@ const loginSuccess = (profile: LoginResult) => {
   const memberStore = useMemberStore()
   memberStore.setProfile(profile)
   // 成功提示
-  uni.showToast({ icon: 'none', title: '登陆成功' })
+  uni.showToast({ icon: 'success', title: '登陆成功' })
   setTimeout(() => {
     // 页面跳转
     uni.switchTab({ url: '/pages/my/my' })
