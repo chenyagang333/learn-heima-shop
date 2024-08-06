5eece2af11502a465761e4cbb226018f58057f98
chenyagang <2578036363@qq.com>
2024-08-06 19:04:41 +0800

    refactor: 更新部分跨端兼容代码

--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -4,11 +4,14 @@ import { useMemberStore } from '@/stores'
 import type { LoginResult } from '@/types/member'
 import { onLoad } from '@dcloudio/uni-app'
 
+// #ifdef MP-WEIXIN
 // 获取 code 登陆凭证
 let code = ''
 onLoad(async () => {
+  // #ifdef MP-WEIXIN
   const res = await wx.login()
   code = res.code
+  // #endif
 })
 
 // 获取用户手机号码（企业中写法）
@@ -20,6 +23,8 @@ const onGetphonenumber: UniHelper.ButtonOnGetphonenumber = async (ev) => {
   const res = await postLoginWxMinAPI({ code, encryptedData, iv })
   loginSuccess(res.result)
 }
+// #endif
+
 // 模拟手机号快捷登录（开发练习）
 const onGetphonenumberSimple = async () => {
   const res = await postLoginWxMinSimpleAPI('17796826259')
@@ -49,15 +54,18 @@ const loginSuccess = (profile: LoginResult) => {
     </view>
     <view class="login">
       <!-- 网页端表单登录 -->
-      <!-- <input class="input" type="text" placeholder="请输入用户名/手机号码" /> -->
-      <!-- <input class="input" type="text" password placeholder="请输入密码" /> -->
-      <!-- <button class="button phone">登录</button> -->
-
+      <!-- #ifdef H5 -->
+      <input class="input" type="text" placeholder="请输入用户名/手机号码" />
+      <input class="input" type="text" password placeholder="请输入密码" />
+      <button class="button phone">登录</button>
+      <!-- #endif -->
       <!-- 小程序端授权登录 -->
+      <!-- #ifdef MP-WEIXIN -->
       <button class="button phone" open-type="getPhoneNumber" @getphonenumber="onGetphonenumber">
         <text class="icon icon-phone"></text>
         手机号快捷登录
       </button>
+      <!-- #endif -->
       <view class="extra">
         <view class="caption">
           <text>其他登录方式</text>
