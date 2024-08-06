c1becc0250d3fc7b7e48ab4ee9d05139a8f99368
chenyagang <2578036363@qq.com>
2024-08-06 17:47:02 +0800

    feat: 订单列表-订单支付

--- src/pagesOrder/list/components/OrderList.vue
+++ src/pagesOrder/list/components/OrderList.vue
@@ -35,12 +35,12 @@
       <view class="action">
         <!-- 待付款状态：显示去支付按钮 -->
         <template v-if="order.orderState === OrderState.DaiFuKuan">
-          <view class="button primary">去支付</view>
+          <view class="button primary" @tap="onOrderPay(order.id)">去支付</view>
         </template>
         <template v-else>
           <navigator
             class="button secondary"
-            :url="`/pagesOrder/create/create?orderId=id`"
+            :url="`/pagesOrder/create/create?orderId=${order.id}`"
             hover-class="none"
           >
             再次购买
@@ -64,6 +64,7 @@ import type { OrderItem, OrderListParams } from '@/types/order'
 import { getMemberOrderAPI } from '@/services/order'
 import { onMounted, ref } from 'vue'
 import { OrderState, orderStateList } from '@/services/constants'
+import { getPayMockAPI, getPayWxPayMiniPayAPI } from '@/services/pay'
 
 // 获取屏幕边界到安全区域距离
 const { safeAreaInsets } = uni.getSystemInfoSync()
@@ -89,6 +90,23 @@ const getMemberOrderData = async () => {
 onMounted(() => {
   getMemberOrderData()
 })
+
+// 订单支付
+const onOrderPay = async (id: string) => {
+  if (import.meta.env.DEV) {
+    // 开发环境模拟支付
+    await getPayMockAPI({ orderId: id })
+  } else {
+    // 正式环境微信支付
+    const res = await getPayWxPayMiniPayAPI({ orderId: id })
+    wx.requestPayment(res.result)
+  }
+  // 成功提示
+  uni.showToast({ title: '支付成功' })
+  // 更新订单状态
+  const order = orderList.value.find((x) => x.id === id)
+  order!.orderState = OrderState.DaiShouHuo
+}
 </script>
 
 <style lang="scss">
